87dadd4d90d974b721492788cb200f973f610a92
furgelisherpa <CoffeeWise@proton.me>
2023-03-26 15:40:39 +0545

    anysize patched

--- config.h
+++ config.h
@@ -5,8 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
-static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
+static char *font = "Ubuntu Mono:pixelsize=14:antialias=true:autohint=true";
+static char *font2[] = { "SymbolsNerdFont:pixelsize=10:antialias=true:autohint=true", 
+                        "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
